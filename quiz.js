294bed8e38c1a5f84a3d2b1084f86148174608d3
Micaela Linda Villani <villani.micaela@gmail.com>
2017-08-31 10:46:28 +0200

    refactor files and code

---
+++ quiz.js
@@ -0,0 +1,22 @@
+function QuizGame(){
+  
+  this.questions = [
+    {
+      question: "What do you love most about showering?",
+      answers: [
+        "I can quietly reflect on the meaning of life.",
+        "It\'s how I get my best ideas!",
+        "It gets the job done.",
+        "What's a shower?"
+      ],
+      answerScore: [
+        5,
+        2,
+        1,
+        0
+      ]
+    }, 
+    {
+      
+    }
+  ]
